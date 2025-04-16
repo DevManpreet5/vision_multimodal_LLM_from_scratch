e45f0ab5636d7e517c2c391a2d129134a1357557
Manpreet Singh <singhman2005123@gmail.com>
2025-04-17 01:10:31 +0530

    added code for siglip embedding

--- modeling_siglip.py
+++ modeling_siglip.py
@@ -43,4 +43,39 @@ class SiglipVisionModel(nn.Module):
 
     def forward(self, pixel_values) :
         return self.vision_model(pixel_values=pixel_values) 
+    
+
+class siglipEmbedding(nn.Module):
+    def __init__(self,config):
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
+
+        self.patch_embedding = nn.Conv2d(
+            in_channels=config.num_channels,
+            out_channels=self.embed_dim,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+            padding="valid", 
+        )
+
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_positions = self.num_patches
+        self.position_embedding = nn.Embedding(self.num_positions, self.embed_dim)
+        self.register_buffer(
+            "position_ids",
+            torch.arange(self.num_positions).expand((1, -1)),
+            persistent=False,
+        )
+
+    def forward(self, pixel_values):
+        
+        patch_embeds = self.patch_embedding(pixel_values)  
+        embeddings = patch_embeds.flatten(2)
+        embeddings = embeddings.transpose(1, 2)
+        embeddings = embeddings + self.position_embedding(self.position_ids)
+        return embeddings
+
+
         
